00af43f866625988fd5a76b6264dcddd32d74a07
kadosh1000 <kadosh10001@gmail.com>
2019-06-04 15:11:11 +0300

    * create a single method for generting gcp auth client

--- src/app.js
+++ src/app.js
@@ -1,13 +1,7 @@
 const { google } = require('googleapis');
 const { JWT } = require('google-auth-library');
 
-function attachUserToResource(action, settings) {
-
-    let cloudResourceManager = google.cloudresourcemanager('v1');
-    let resourceId = action.params.RESOURCEID;
-    let userEmail = action.params.EMAIL;
-    let role = action.params.ROLE;
-    
+function _createAuthClient(action,settings){
     let keysParam = action.params.CREDENTIALS || settings.CREDENTIALS
     let keys;
 
@@ -17,18 +11,27 @@ function attachUserToResource(action, settings) {
         try{
             keys = JSON.parse(keysParam)
         }catch(err){
-            return Promise.reject("Invalid credentials JSON");
+            throw new Error("Invalid credentials JSON");
         }
     }
-    
+
+    return new JWT(
+        keys.client_email,
+        null,
+        keys.private_key,
+        ['https://www.googleapis.com/auth/cloud-platform'],
+    );
+}
+
+function attachUserToResource(action, settings) {
+
+    let cloudResourceManager = google.cloudresourcemanager('v1');
+    let resourceId = action.params.RESOURCEID;
+    let userEmail = action.params.EMAIL;
+    let role = action.params.ROLE;
 
     return new Promise((resolve, reject) => {
-        const client = new JWT(
-            keys.client_email,
-            null,
-            keys.private_key,
-            ['https://www.googleapis.com/auth/cloud-platform'],
-        );
+        const client = _createAuthClient(action,settings);
 
         var request = {
             resource_: resourceId,
@@ -75,28 +78,8 @@ function setIamPolicy(action, settings) {
     let userEmail = action.params.EMAIL;
     let role = action.params.ROLE;
 
-    let keysParam = action.params.CREDENTIALS || settings.CREDENTIALS
-    let keys;
-
-    if (typeof keysParam != 'string'){
-        keys = keysParam;
-    } else {
-        try{
-            keys = JSON.parse(keysParam)
-        }catch(err){
-            return Promise.reject("Invalid credentials JSON");
-        }
-    }
-
-
     return new Promise((resolve, reject) => {
-        const client = new JWT(
-            keys.client_email,
-            null,
-            keys.private_key,
-            ['https://www.googleapis.com/auth/cloud-platform'],
-        );
-
+        const client = _createAuthClient(action,settings);
         let request = {
             resource_: resourceId,
             resource: {
