6a8fcd314cb1389a9a5a9ed654198657e8695e41
Amit Alon <amit@kaholo.io>
2021-12-20 12:16:03 +0200

    added category and keywords

--- src/helpers.js
+++ src/helpers.js
@@ -1,6 +1,5 @@
 function removeUndefinedAndEmpty(obj){
     Object.entries(obj).forEach(([key, value]) => {
-        if (key === "auth") return;
         if (value === undefined) delete obj[key];
         if (Array.isArray(value) && value.length === 0) delete obj[key];
         if (typeof(value) === 'object'){
